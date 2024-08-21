43a7572a1a7749e07d12aa2ca9b48fb2d88f7ddd
Bebo-91 <abanobhany91@gmail.com>
2024-08-22 01:03:34 +0300

    init ProductDetails.tsx

---
+++ Screens/FinalProject/ProductDetails.tsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import {
+    View,
+    Text,
+    TextInput,
+    FlatList,
+    TouchableOpacity,
+    StyleSheet,
+    SafeAreaView,
+    Button,
+    Image,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Products() {
+    return (
+        <View>
+
+        </View>
+    )
+}
